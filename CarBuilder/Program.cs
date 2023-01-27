b4a7be7de6cc0c425284221185cd60fd44f4efe8
Anna Metsäpelto <anna.metsapelto@gmail.com>
2023-01-27 09:08:06 +0200

    Create instance and test it

--- CarBuilder/Program.cs
+++ CarBuilder/Program.cs
@@ -1,5 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Motor SmallMotor = new Motor("Type 1", 2000, 500);
 
+Car Volvo1 = new Volvo(CarType.passenger, 2020, SmallMotor, "red", "V60", 5, FuelType.gasoline);
+
+Console.WriteLine("The color of the car is " + Volvo1.Color + ". It uses " + Volvo1.Fuel + " as fuel.");
 
 
